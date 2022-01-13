13188e142a804ce80e157a8642b6630286ea1c5f
Renier <Renierhechter25@gmail.com>
2022-01-13 09:53:02 +0200

    Add project files.

---
+++ MyFirstWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyFirstWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Renier this is your first Web Application, Congratulations!";
+            ViewBag.Button = "Click here to  Continue.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewBag.Message = "Security is everybody`s business.";
+            ViewBag.MyFavoriteColor = "Blue";
+            ViewBag.Button2 = "Why did you not click there?";
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
